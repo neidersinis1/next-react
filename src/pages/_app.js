9d53398b3aa11e9ecab76eedea6eb36d774fffd0
Neider Sinisterra <neidersinis@hotmail.com>
2022-07-30 10:26:16 -0500

    agregamos .env-example, hooks, layout, login.js, services.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,17 @@
+import MainLayout from '@layout/MainLayout';
+import { ProviderAuth } from '@hooks/useAuth';
 import '@styles/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <ProviderAuth>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </ProviderAuth>
+    </>
+  );
 }
 
 export default MyApp;
