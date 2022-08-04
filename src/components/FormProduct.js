834d1ea4a0ead0c7a8c3e375aded10ed6f24fab1
Neider Sinisterra <neidersinis@hotmail.com>
2022-08-04 11:44:52 -0500

    ya podemos traer los valores de edit para editarlos.

--- src/components/FormProduct.js
+++ src/components/FormProduct.js
@@ -2,8 +2,8 @@ import { useRef } from 'react';
 import { addProduct } from '@services/api/products';
 
 
-export default function FormProduct( { setOpen, setAlert } ) {
-    const formRef = useRef(null); //null como primera instancia
+export default function FormProduct( { setOpen, setAlert, product } ) {
+  const formRef = useRef(null); //null como primera instancia
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -46,19 +46,20 @@ export default function FormProduct( { setOpen, setAlert } ) {
               <label htmlFor="title" className="block text-sm font-medium text-gray-700">
                 Title
               </label>
-              <input type="text" name="title" id="title" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+              <input defaultValue={product?.title} type="text" name="title" id="title" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
             </div>
             <div className="col-span-6 sm:col-span-3">
               <label htmlFor="price" className="block text-sm font-medium text-gray-700">
                 Price
               </label>
-              <input type="number" name="price" id="price" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+              <input defaultValue={product?.price} type="number" name="price" id="price" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
             </div>
             <div className="col-span-6">
               <label htmlFor="category" className="block text-sm font-medium text-gray-700">
                 Category
               </label>
               <select
+                defaultValue={product?.category}
                 id="category"
                 name="category"
                 autoComplete="category-name"
@@ -77,6 +78,7 @@ export default function FormProduct( { setOpen, setAlert } ) {
                 Description
               </label>
               <textarea
+              defaultValue={product?.description}
                 name="description"
                 id="description"
                 autoComplete="description"
@@ -103,7 +105,7 @@ export default function FormProduct( { setOpen, setAlert } ) {
                         className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
                       >
                         <span>Upload a file</span>
-                        <input id="images" name="images" type="file" className="sr-only" />
+                        <input defaultValue={product?.images} id="images" name="images" type="file" className="sr-only" />
                       </label>
                       <p className="pl-1">or drag and drop</p>
                     </div>
